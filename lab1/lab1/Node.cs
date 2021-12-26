7652f5702967049609b3a14466cf5c44405f8e9b
DzividzinskaM <marianadzividzinska04@gmail.com>
2021-12-26 23:28:01 +0200

    lab1: relocate node file to root folder

---
+++ lab1/lab1/Node.cs
@@ -0,0 +1,24 @@
+﻿using Akka.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{ 
+    public class Node<T>
+    { 
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node()
+        {
+            Next = null;
+        }
+
+        public Node(T data)
+        {
+            //Next = null;
+            Data = data;
+        }
+    }
+}
